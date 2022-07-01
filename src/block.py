147cbc43ec0a802ac0361c10183a45f555978e18
Ayush <ayushbheemaiah@gmail.com>
2022-06-30 17:26:29 -0700

    Simple Clicker Game

--- src/block.py
+++ src/block.py
@@ -7,17 +7,23 @@ class Block(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.sprite_sheet=pygame.image.load('./resources/Spritesheet.png')
         self.load_sprites()
-        self.sprite=self.sprites['Dirt']
+        self.sprite=self.sprites[0]
+        self.number = 0
+        
     def load_sprites(self):
-        self.sprites = {}
-        self.sprites['Dirt'] = utl.load_sprite(self.sprite_sheet, 310, 27, 73, 73)
-        self.sprites['Grass'] = utl.load_sprite(self.sprite_sheet, 237, 27, 73, 73)
-        self.sprites['Stone'] = utl.load_sprite(self.sprite_sheet, 383, 27, 73, 73) 
-        self.sprites['Coal'] = utl.load_sprite(self.sprite_sheet, 310, 173, 73, 73)
-        self.sprites['Gold'] = utl.load_sprite(self.sprite_sheet, 456, 100, 73, 73)
-        self.sprites['Redstone'] = utl.load_sprite(self.sprite_sheet, 602, 100, 73, 73)
-        self.sprites['Diamond'] = utl.load_sprite(self.sprite_sheet, 529, 100, 73, 73)
+        self.sprites = []
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 310, 27, 73, 73)) 
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 237, 27, 73, 73))
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 383, 27, 73, 73) )
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 310, 173, 73, 73))
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 456, 100, 73, 73))
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 602, 100, 73, 73))
+        self.sprites.append(utl.load_sprite(self.sprite_sheet, 529, 100, 73, 73))
         
-        for state in self.sprites.keys():
-            self.sprites[state] = pygame.transform.scale2x(self.sprites[state])
+        for i in range(len(self.sprites)):
+            self.sprites[i] = pygame.transform.scale2x(self.sprites[i])
         
+    def upgrade(self):
+        if self.number < 6:
+            self.number +=1
+            self.sprite = self.sprites[self.number]
--- src/main.py
+++ src/main.py
@@ -4,16 +4,25 @@ from block import *
 
 score = 0
 
+button = pygame.image.load("./resources/button.png")
+button = pygame.transform.scale(button, (50, 50))
+
+upgradeCost = 1
+clickingPower = 1
+
+
 def init():
+    global screen, font, block, score
     pg.init()
     screen = pg.display.set_mode((c.WIDTH, c.HEIGHT))
+   
     pg.display.set_caption('Dirt Clicker')
     screen.fill('white')
     print('Beginning event logging...')
     print()
     block = Block()
-    screen.blit(block.sprite, (400, 300))
-    font=pg.font.FONT(pg.font.get_default_font(),16)
+    
+    font=pg.font.Font(pg.font.get_default_font(),16)
     print('Game Start')
 
 def main():
@@ -25,15 +34,29 @@ def main():
                 running = False
                 
             if event.type == pg.MOUSEBUTTONDOWN:
-                handleclick()
+                handleclick(event.pos)
                     
         pg.display.update()
+        screen.fill("white")
+        screen.blit(block.sprite, (400, 300))
+        screen.blit(font.render(str(score), False, "black"),(600, 350))
+        screen.blit((button),(300, 330))
     pg.quit()
     print("Window closed, game successfully quit")
 
-def handleclick(x, y):
+def handleclick(pos):
+    global clickingPower, upgradeCost, score
+    x, y = pos
     if x in range(400, 546) and y in range(300, 446):
-        score +=1
+        score += clickingPower
+        
+    if x in range(300, 300 + button.get_width()) and y in range (330, 330 + button.get_height()):
+        if score >= upgradeCost: 
+            clickingPower = clickingPower*2
+            score-=upgradeCost
+            upgradeCost = upgradeCost*6
+            block.upgrade()
+            
     
 
 main()
